f4836b9271d5d25e543d1a37077130a0304233f5
ImaginaryResources <61711375+ImaginaryResources@users.noreply.github.com>
2020-03-02 12:53:46 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,150 @@
+/**
+ 
+ Copyright 2016 Brian Donohue.
+ 
+ Used and Modified by ImaginaryResources 3/14/18
+ 
+*/
+
+'use strict';
+
+var powers = ["Flying", "Strength", "Teleportation", "Invisibility", "Morphing", "Laser Eyes", "Super Speed", "Telekinesis", "Self Healing", "Mind Reading", "Super Intelligence"];
+
+var randNum = parseInt(0);
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // add any session init logic here
+}
+
+/**
+ * Called when the user invokes the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var cardTitle = "Random Super Power"
+    var speechOutput = "Try, Alexa ask Random Super Power"
+    callback(session.attributes,
+        buildSpeechletResponse(cardTitle, speechOutput, "", true));
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+
+    // dispatch custom intents to handlers here
+    if (intentName == 'RandomPowerIntent'){
+        handleRandomPowerRequest(intent, session, callback);
+    }
+    
+    else {
+        throw "Invalid intent";
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // Add any cleanup logic here
+}
+
+function handleRandomPowerRequest(intent, session, callback) {
+    randNum = Math.floor(Math.random()*11);
+    callback(session.attributes, buildSpeechletResponseWithoutCard("You are granted the power of "+ powers[randNum], "", "true"));
+}
+
+// ------- Helper functions to build responses -------
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
